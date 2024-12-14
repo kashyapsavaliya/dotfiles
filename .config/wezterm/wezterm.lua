101cb9f9acad21a6ff0301ed6c7b32b5249d6da3
Kashyap Savaliya <savaliyak97@gmail.com>
2024-12-14 16:49:18 -0500

    chore(wezterm): switch to GruvboxDarkHard

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- Changing the color scheme
-config.color_scheme = 'Gruvbox dark, hard (base16)'
+config.color_scheme = 'GruvboxDarkHard'
 
 -- Font size
 config.font_size = 22.5
