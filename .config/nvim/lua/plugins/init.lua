c1a4a537642417269cf2c451ce4a63ef1da0f60d
Kashyap Savaliya <savaliyak97@gmail.com>
2026-04-09 19:17:30 -0400

    feat(nvim): add render-markdown

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,13 +2,16 @@ local lualine = require('plugins.lualine')
 local colorscheme = require('plugins.colorscheme')
 local nvimtree = require('plugins.nvim-tree')
 local whichkey = require('plugins.which-key')
+local rendermd = require('plugins.render-markdown')
 
 local plugins = { 
   "https://github.com/nvim-tree/nvim-web-devicons", -- Add this for lualine
+  "https://github.com/nvim-treesitter/nvim-treesitter",
   colorscheme, 
   lualine,
   nvimtree,
   whichkey,
+  rendermd,
 }
 
 vim.pack.add(plugins)
---
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,8 @@
+return {
+  src = "https://github.com/MeanderingProgrammer/render-markdown.nvim",
+  name = "render-markdown",
+
+  setup = function()
+    require("render-markdown").setup({})
+  end,
+}
