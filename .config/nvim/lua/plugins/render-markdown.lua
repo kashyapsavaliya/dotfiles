c4c804c34887ab6ff47a164df6bfe5142d1ad742
Kashyap Savaliya <savaliyak97@gmail.com>
2026-04-09 20:03:11 -0400

    feat(nvim): add render-markdown config

--- .config/nvim/lua/plugins/render-markdown.lua
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -3,6 +3,231 @@ return {
   name = "render-markdown",
 
   setup = function()
-    require("render-markdown").setup({})
+    require("render-markdown").setup({
+      link = {
+        -- Turn on / off inline link icon rendering.
+        enabled = true,
+        -- Additional modes to render links.
+        render_modes = false,
+        -- How to handle footnote links, start with a '^'.
+        footnote = {
+            -- Turn on / off footnote rendering.
+            enabled = true,
+            -- Inlined with content.
+            icon = '󰯔 ',
+            -- Custom processing for footnote body to show.
+            -- Runs before prefix / suffix are added and superscript processing.
+            body = function(ctx)
+                return ctx.text
+            end,
+            -- Replace value with superscript equivalent.
+            superscript = true,
+            -- Added before link content.
+            prefix = '',
+            -- Added after link content.
+            suffix = '',
+        },
+        -- Inlined with 'image' elements.
+        image = '󰥶 ',
+        -- Check custom for 'image' elements.
+        image_custom = true,
+        -- Inlined with 'email_autolink' elements.
+        email = '󰀓 ',
+        -- Fallback icon for 'inline_link' and 'uri_autolink' elements.
+        hyperlink = '󰌹 ',
+        -- Applies to the inlined icon as a fallback.
+        highlight = 'RenderMarkdownLink',
+        -- Applies to the link title.
+        highlight_title = 'RenderMarkdownLinkTitle',
+        -- Applies to WikiLink elements.
+        wiki = {
+            -- Turn on / off WikiLink rendering.
+            enabled = true,
+            -- Inlined with content.
+            icon = '󱗖 ',
+            -- Custom processing for WikiLink body to show.
+            body = function()
+                return nil
+            end,
+            -- Applies to the inlined icon.
+            highlight = 'RenderMarkdownWikiLink',
+            -- Highlight for item associated with the WikiLink.
+            scope_highlight = nil,
+        },
+        -- Define custom destination patterns so icons can quickly inform you of what a link
+        -- contains. Applies to 'image', 'inline_link', 'uri_autolink', and WikiLink nodes.
+        -- When multiple patterns match a link the one with the longer pattern is used.
+        -- The key is for healthcheck and to allow users to change its values, value type below.
+        -- | pattern   | matched against the destination text                            |
+        -- | icon      | gets inlined before the link text                               |
+        -- | kind      | optional determines how pattern is checked                      |
+        -- |           | pattern | @see :h lua-patterns, is the default if not set       |
+        -- |           | suffix  | @see :h vim.endswith()                                |
+        -- | priority  | optional used when multiple match, uses pattern length if empty |
+        -- | highlight | optional highlight for 'icon', uses fallback highlight if empty |
+        custom = {
+            web = { pattern = '^http', icon = '󰖟 ' },
+            apple = { pattern = 'apple%.com', icon = ' ' },
+            discord = { pattern = 'discord%.com', icon = '󰙯 ' },
+            github = { pattern = 'github%.com', icon = '󰊤 ' },
+            google = { pattern = 'google%.com', icon = '󰊭 ' },
+            neovim = { pattern = 'neovim%.io', icon = ' ' },
+            reddit = { pattern = 'reddit%.com', icon = '󰑍 ' },
+            x = { pattern = 'x%.com', icon = ' ' },
+            youtube = { pattern = 'youtube[^.]*%.com', icon = '󰗃 ' },
+        },
+    },
+    callout = {
+        -- Callouts are a special instance of a 'block_quote' that start with a 'shortcut_link'.
+        -- The key is for healthcheck and to allow users to change its values, value type below.
+        -- | raw        | matched against the raw text of a 'shortcut_link', case insensitive |
+        -- | rendered   | replaces the 'raw' value when rendering                             |
+        -- | highlight  | highlight for the 'rendered' text and quote markers                 |
+        -- | quote_icon | optional override for quote.icon value for individual callout       |
+        -- | category   | optional metadata useful for filtering                              |
+
+        note      = { raw = '[!NOTE]',      rendered = '󰋽 Note',      highlight = 'RenderMarkdownInfo',    category = 'github'   },
+        tip       = { raw = '[!TIP]',       rendered = '󰌶 Tip',       highlight = 'RenderMarkdownSuccess', category = 'github'   },
+        important = { raw = '[!IMPORTANT]', rendered = '󰅾 Important', highlight = 'RenderMarkdownHint',    category = 'github'   },
+        warning   = { raw = '[!WARNING]',   rendered = '󰀪 Warning',   highlight = 'RenderMarkdownWarn',    category = 'github'   },
+        caution   = { raw = '[!CAUTION]',   rendered = '󰳦 Caution',   highlight = 'RenderMarkdownError',   category = 'github'   },
+        -- Obsidian: https://help.obsidian.md/Editing+and+formatting/Callouts
+        abstract  = { raw = '[!ABSTRACT]',  rendered = '󰨸 Abstract',  highlight = 'RenderMarkdownInfo',    category = 'obsidian' },
+        summary   = { raw = '[!SUMMARY]',   rendered = '󰨸 Summary',   highlight = 'RenderMarkdownInfo',    category = 'obsidian' },
+        tldr      = { raw = '[!TLDR]',      rendered = '󰨸 Tldr',      highlight = 'RenderMarkdownInfo',    category = 'obsidian' },
+        info      = { raw = '[!INFO]',      rendered = '󰋽 Info',      highlight = 'RenderMarkdownInfo',    category = 'obsidian' },
+        todo      = { raw = '[!TODO]',      rendered = '󰗡 Todo',      highlight = 'RenderMarkdownInfo',    category = 'obsidian' },
+        hint      = { raw = '[!HINT]',      rendered = '󰌶 Hint',      highlight = 'RenderMarkdownSuccess', category = 'obsidian' },
+        success   = { raw = '[!SUCCESS]',   rendered = '󰄬 Success',   highlight = 'RenderMarkdownSuccess', category = 'obsidian' },
+        check     = { raw = '[!CHECK]',     rendered = '󰄬 Check',     highlight = 'RenderMarkdownSuccess', category = 'obsidian' },
+        done      = { raw = '[!DONE]',      rendered = '󰄬 Done',      highlight = 'RenderMarkdownSuccess', category = 'obsidian' },
+        question  = { raw = '[!QUESTION]',  rendered = '󰘥 Question',  highlight = 'RenderMarkdownWarn',    category = 'obsidian' },
+        help      = { raw = '[!HELP]',      rendered = '󰘥 Help',      highlight = 'RenderMarkdownWarn',    category = 'obsidian' },
+        faq       = { raw = '[!FAQ]',       rendered = '󰘥 Faq',       highlight = 'RenderMarkdownWarn',    category = 'obsidian' },
+        attention = { raw = '[!ATTENTION]', rendered = '󰀪 Attention', highlight = 'RenderMarkdownWarn',    category = 'obsidian' },
+        failure   = { raw = '[!FAILURE]',   rendered = '󰅖 Failure',   highlight = 'RenderMarkdownError',   category = 'obsidian' },
+        fail      = { raw = '[!FAIL]',      rendered = '󰅖 Fail',      highlight = 'RenderMarkdownError',   category = 'obsidian' },
+        missing   = { raw = '[!MISSING]',   rendered = '󰅖 Missing',   highlight = 'RenderMarkdownError',   category = 'obsidian' },
+        danger    = { raw = '[!DANGER]',    rendered = '󱐌 Danger',    highlight = 'RenderMarkdownError',   category = 'obsidian' },
+        error     = { raw = '[!ERROR]',     rendered = '󱐌 Error',     highlight = 'RenderMarkdownError',   category = 'obsidian' },
+        bug       = { raw = '[!BUG]',       rendered = '󰨰 Bug',       highlight = 'RenderMarkdownError',   category = 'obsidian' },
+        example   = { raw = '[!EXAMPLE]',   rendered = '󰉹 Example',   highlight = 'RenderMarkdownHint' ,   category = 'obsidian' },
+        quote     = { raw = '[!QUOTE]',     rendered = '󱆨 Quote',     highlight = 'RenderMarkdownQuote',   category = 'obsidian' },
+        cite      = { raw = '[!CITE]',      rendered = '󱆨 Cite',      highlight = 'RenderMarkdownQuote',   category = 'obsidian' },
+    },
+    checkbox = {
+        -- Checkboxes are a special instance of a 'list_item' that start with a 'shortcut_link'.
+        -- There are two special states for unchecked & checked defined in the markdown grammar.
+
+        -- Turn on / off checkbox state rendering.
+        enabled = true,
+        -- Additional modes to render checkboxes.
+        render_modes = false,
+        -- Render the bullet point before the checkbox.
+        bullet = false,
+        -- Padding to add to the left of checkboxes.
+        left_pad = 0,
+        -- Padding to add to the right of checkboxes.
+        right_pad = 1,
+        unchecked = {
+            -- Replaces '[ ]' of 'task_list_marker_unchecked'.
+            icon = '󰄱 ',
+            -- Highlight for the unchecked icon.
+            highlight = 'RenderMarkdownUnchecked',
+            -- Highlight for item associated with unchecked checkbox.
+            scope_highlight = nil,
+        },
+        checked = {
+            -- Replaces '[x]' of 'task_list_marker_checked'.
+            icon = '󰱒 ',
+            -- Highlight for the checked icon.
+            highlight = 'RenderMarkdownChecked',
+            -- Highlight for item associated with checked checkbox.
+            scope_highlight = nil,
+        },
+        -- Define custom checkbox states, more involved, not part of the markdown grammar.
+        -- As a result this requires neovim >= 0.10.0 since it relies on 'inline' extmarks.
+        -- The key is for healthcheck and to allow users to change its values, value type below.
+        -- | raw             | matched against the raw text of a 'shortcut_link'           |
+        -- | rendered        | replaces the 'raw' value when rendering                     |
+        -- | highlight       | highlight for the 'rendered' icon                           |
+        -- | scope_highlight | optional highlight for item associated with custom checkbox |
+        -- stylua: ignore
+        custom = {
+            todo = { raw = '[-]', rendered = '󰥔 ', highlight = 'RenderMarkdownTodo', scope_highlight = nil },
+        },
+        -- Priority to assign to scope highlight.
+        scope_priority = nil,
+    },
+    quote = {
+        -- Turn on / off block quote & callout rendering.
+        enabled = true,
+        -- Additional modes to render quotes.
+        render_modes = false,
+        -- Replaces '>' of 'block_quote'.
+        icon = '▋',
+        -- Whether to repeat icon on wrapped lines. Requires neovim >= 0.10. This will obscure text
+        -- if incorrectly configured with :h 'showbreak', :h 'breakindent' and :h 'breakindentopt'.
+        -- A combination of these that is likely to work follows.
+        -- | showbreak      | '  ' (2 spaces)   |
+        -- | breakindent    | true              |
+        -- | breakindentopt | '' (empty string) |
+        -- These are not validated by this plugin. If you want to avoid adding these to your main
+        -- configuration then set them in win_options for this plugin.
+        repeat_linebreak = false,
+        -- Highlight for the quote icon.
+        -- If a list is provided output is evaluated by `cycle(value, level)`.
+        highlight = {
+            'RenderMarkdownQuote1',
+            'RenderMarkdownQuote2',
+            'RenderMarkdownQuote3',
+            'RenderMarkdownQuote4',
+            'RenderMarkdownQuote5',
+            'RenderMarkdownQuote6',
+        },
+    },
+    bullet = {
+        -- Useful context to have when evaluating values.
+        -- | level | how deeply nested the list is, 1-indexed          |
+        -- | index | how far down the item is at that level, 1-indexed |
+        -- | value | text value of the marker node                     |
+
+        -- Turn on / off list bullet rendering
+        enabled = true,
+        -- Additional modes to render list bullets
+        render_modes = false,
+        -- Replaces '-'|'+'|'*' of 'list_item'.
+        -- If the item is a 'checkbox' a conceal is used to hide the bullet instead.
+        -- Output is evaluated depending on the type.
+        -- | function   | `value(context)`                                    |
+        -- | string     | `value`                                             |
+        -- | string[]   | `cycle(value, context.level)`                       |
+        -- | string[][] | `clamp(cycle(value, context.level), context.index)` |
+        icons = { '●', '○', '◆', '◇' },
+        -- Replaces 'n.'|'n)' of 'list_item'.
+        -- Output is evaluated using the same logic as 'icons'.
+        ordered_icons = function(ctx)
+            local value = vim.trim(ctx.value)
+            local index = tonumber(value:sub(1, #value - 1))
+            return ('%d.'):format(index > 1 and index or ctx.index)
+        end,
+        -- Padding to add to the left of bullet point.
+        -- Output is evaluated depending on the type.
+        -- | function | `value(context)` |
+        -- | integer  | `value`          |
+        left_pad = 0,
+        -- Padding to add to the right of bullet point.
+        -- Output is evaluated using the same logic as 'left_pad'.
+        right_pad = 0,
+        -- Highlight for the bullet icon.
+        -- Output is evaluated using the same logic as 'icons'.
+        highlight = 'RenderMarkdownBullet',
+        -- Highlight for item associated with the bullet point.
+        -- Output is evaluated using the same logic as 'icons'.
+        scope_highlight = {},
+        -- Priority to assign to scope highlight.
+        scope_priority = nil,
+      },
+    })
   end,
 }
